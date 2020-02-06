2a1cc2ca4efbef1f3129b74f7ccd1c0dd1e5b717
Eric Howell <eric.howell@ogilvy.com>
2020-02-06 11:08:40 -0800

    Refactored  zipping task

--- gulpfile.js
+++ gulpfile.js
@@ -10,32 +10,29 @@ const gulp = require('gulp'),
 			gch = require('gulp-compile-handlebars'),
 			gzip = require('gulp-zip');
 
-const config = require('./config.json');
+const config = require('./src/config.json');
 
-const srcPath = './src/banners',
-			partialsPath ='./src/lib',
-			distPath = './dist',
-			srcFolders = getFolders(srcPath);
+const paths = {
+	src:'./src/banners',
+	dist:'./dist',
+	partials:'./src/lib',
+	zips: './dist/zips'
+}
+
+const srcFolders = getFolders(paths.src);
 
 // Watch All Project Source & Libray Files For Changes
-const watcher = () => {gulp.watch([srcPath+'/**','./src/lib/**/*'],build)}
+const watcher = () => {gulp.watch([paths.src+'/**/*', paths.partials+'/**/*'],build)}
 const zipper = gulp.series(build, zipFiles);
 
 gulp.task('zip', zipper);
 gulp.task('watch', watcher);
 gulp.task('default', build);
 
-/*function getName() {return this._name; }
-function getJsName() {return this._name+'.js';}
-function getCssName() {return this._name+'.css';}
-function getSharedJs() {return this.sharedFile+'.js';} 
-function getSharedCss() {return this.sharedFile+'.css'; }
-function getEndLogo() { return this.endLogo }*/
-
 function build(){
 	let task = srcFolders.map(function(folder) {
-		let _src = path.join(srcPath, folder),// Path source files
-				_dist = mkDirByPathSync(path.join(distPath, folder)),//Path to final production files
+		let _src = path.join(paths.src, folder),// Path source files
+				_dist = mkDirByPathSync(path.join(paths.dist, folder)),//Path to final production files
 				_imgPath = mkDirByPathSync(path.join(_dist, '/images/')),
 				_name = path.basename(folder), 
 				_data = config[_name];
@@ -45,7 +42,7 @@ function build(){
 		// Configure gulp-compile-handlebars options
 		let option = {
 			ignorePartials:false,
-			batch:[_src, partialsPath+'/css', partialsPath+'/html', partialsPath+'/js', partialsPath+'/svg'],
+			batch:[_src, paths.partials+'/css', paths.partials+'/html', paths.partials+'/js', paths.partials+'/svg'],
 			helpers : {
 				fileName	: function(){	return this._name; },
 				bannerJs	: function(){	return this._name+'.js';},
@@ -56,7 +53,7 @@ function build(){
 			}
 		}
 
-		let _html = gulp.src(partialsPath+'/html/*.hbs')
+		let _html = gulp.src(paths.partials+'/html/*.hbs')
 				.pipe(gch(_data, option))
 				.pipe(rename(_name+'.html'))
 				.pipe(gulp.dest(_dist));
@@ -67,14 +64,17 @@ function build(){
 
 		return merge(_images, _html);
 
-		/*let jsOptions = {
+		/*
+		// For exporting JS files.
+
+		let jsOptions = {
 			ignorePartials:false, 
-			batch:[_src, partialsPath+'/js'], 
+			batch:[_src, paths.partials+'/js'], 
 			helpers : { getFileName : getName }
 		}
 		// console.log('•• HANDLEBARS1 •• '+_data._name);
 
-		let _js = gulp.src(partialsPath+'/js/template.hbs')
+		let _js = gulp.src(paths.partials+'/js/template.hbs')
 				.pipe(gch(_data, jsOptions))
 				.pipe(rename(_name+'.js'))
 				.pipe(gulp.dest(_dist));
@@ -88,22 +88,29 @@ function build(){
 // Zip up all final files
 function zipFiles() {
 	let task = srcFolders.map(function(folder) {
-		let _src = path.join('./dist/', folder); // Path to each src folder
-		let _dist = './zips/' // Where to put the zips
-		// Set the source to all files in dist folder
-		let _final =  gulp.src([_src+'/*.html',_src+'/*.css',_src+'/*.js',_src+'/images/**'],{base:_src})
-			.pipe(gzip('_final.zip'))
-			.pipe(rename(function(file){
-				let temp = path.basename(folder);
-				file.basename = temp + file.basename;
-			}))
-			.pipe(gulp.dest(_dist));
-			return _final;
+		let _src = path.join(paths.src, folder),// Path source files
+				_dist = path.join(paths.dist, folder),// Path to production files
+				_dest = paths.zips,
+				_images = _dist+'/images/';// Path to production image files
+		// Package all files in dist folder
+		let _final =  gulp.src([_dist+'/**/*'],{base:_dist})
+			.pipe(zip('_final.zip'))
+			.pipe(rename(function(file){file.basename = folder + file.basename;}))
+			.pipe(gulp.dest(_dest));
+
+		// Package all source files except node_modules 
+		 let _source =  gulp.src(['*.{json,js,txt}', 'lib/**', paths.partials+'/**/*', _src+'/**/*', paths.zips],{base:'./'})
+			.pipe(zip('_source.zip'))
+			.pipe(rename(function(file){file.basename = folder + file.basename;}))
+			.pipe(gulp.dest(_dest));
+
+		let _html = gulp.src(_dist+'/*.html').pipe(gulp.dest(_dest));
+
+		return merge(_final, _html, _source);
 	});
 	let lastStream = task[task.length-1];
 	return lastStream;
-}
-
+};
 
 // Scans basePath for sub directories (individual banner folders)
 function getFolders(dir) {
